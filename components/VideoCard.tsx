d7f40c584e9ccc97b9ecbe55a42bfd075907de86
Andrei Florea <floreaalexandru146@gmail.com>
2022-09-30 20:01:58 +0200

    Small Bugs Fix

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -57,9 +57,8 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
 
         <div className='lg:ml-20 flex gap-4 relative'>
             <div onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)} className='rounded-3xl'>
-                <Link href="/">
-                    <video ref={videoRef} src={post.video.asset.url} loop className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] cursor-pointer rounded-2xl bg-gray-100'>
-
+                <Link href={`/detail/${post._id}`}>
+                    <video ref={videoRef} src={post.video.asset.url} loop className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] sm:w-[400px] w-[200px] cursor-pointer rounded-2xl bg-gray-100'>
                     </video>
                 </Link>
 
