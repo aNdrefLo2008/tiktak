52c56221a730a0d840190fe802872b82d98a4a6d
Andrei Florea <floreaalexandru146@gmail.com>
2022-10-12 16:03:12 +0200

    Finishing Suggested Accounts and Comment Functionalities

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -27,7 +27,7 @@ function Comments({ comment, setComment, addComment, comments, isPostingComment 
 
   return (
     <div className='border-y-2 border-gray-200 bg-[#F8F8F8] lg:pb-0 pb-[100px]'>
-        <div className='overflow-scroll lg:h-[475px]'>
+        <div className='overflow-scroll lg:h-[475px] ml-6 mt-4'>
             {comments?.length ? (
                 comments.map((item, idx) => (
                     <>
@@ -38,22 +38,22 @@ function Comments({ comment, setComment, addComment, comments, isPostingComment 
                             ) && (
                                 <div className='p-2 items-center' key={idx}>
                                     <Link href={`/profile/${user._id}`}>
-                                        <div className='flex items-start'>
-                                            <div className='w-8 h-8'>
-                                                <Image src={user.image} width={34} height={34} className='rounded-full' alt="user profile" layout='responsive'/>
+                                        <div className='flex items-start gap-3 mb-2 cursor-pointer'>
+                                            <div className='w-12 h-12'>
+                                                <Image src={user.image} width={48} height={48} className='rounded-full' alt="user profile" layout='responsive'/>
                                             </div>
 
-                                            <div className='hidden xl:block'>
-                                                <p className='flex gap-1 items-center font-bold text-primary lowercase'>
-                                                {user.userName.replaceAll(' ', '')} 
-                                                <GoVerified className='text-blue-400 '/>
-                                                </p>
-                                                <p className='capitalize text-gray-400 text-xs'>
-                                                {user.userName}
-                                                </p>
-                                            </div>
+                                            <p className='flex cursor-pointer gap-1 items-center text-[18px] font-bold leading-6 text-primary'>
+                                                {user.userName}{' '}
+                                                <GoVerified className='text-blue-400' />
+                                            </p>
+
                                         </div>
                                     </Link>
+
+                                    <div>
+                                        <p>{item.comment}</p>
+                                    </div>
                                 </div>
                             )
                         ))}
